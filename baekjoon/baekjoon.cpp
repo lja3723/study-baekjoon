8d9f11fbbf19fc3e5673a888cf82b0ba6913c6a4
lja3723 <lja3723@gmail.com>
2022-10-17 00:18:32 +0900

    solve 11659

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,33 +1,21 @@
-﻿//problem No. 9184, 신나는 함수 실행
+﻿//problem No. 11659, 구간 합 구하기 4
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int w(int a, int b, int c) {
-    static int m[21][21][21];
-    if (a <= 0 || b <= 0 || c <= 0)
-        return 1;
-    if (a > 20 || b > 20 || c > 20)
-        return w(20, 20, 20);
-    if (m[a][b][c] == 0) {
-        if (a < b && b < c)
-            m[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-        else 
-            m[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-    }    
-    return m[a][b][c];
-}
 
 int main()
 {
-    while (1) {
-        //input
-        int a, b, c;
-        cin >> a >> b >> c;
-        if (a == -1 && b == -1 &&  c == -1) break;
-
-        //process
-        //output
-        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
-    }
-
+	cin.tie(0);
+	int N, M; 
+	cin >> N >> M;
+	vector<int> v(N + 1);
+	vector<int> s(N + 1);
+	for (int i = 1; i <= N; i++) {
+		cin >> v[i];
+		s[i] = s[i - 1] + v[i];
+	}
+	while (M--) {
+		int i, j; cin >> i >> j;
+		cout << s[j] - s[i - 1] << "\n";
+	}
 }
