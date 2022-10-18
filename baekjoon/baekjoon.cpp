e0b28c875f65ee8f9c3c631ff3c90033eda74b8e
lja3723 <lja3723@gmail.com>
2022-10-18 18:54:54 +0900

    solve 15651

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,53 +1,30 @@
-﻿//problem No. 24060, 알고리즘 수업 - 병합 정렬 1
+﻿//problem No. 15651, N과 M (3)
 #include <iostream>
 #include <vector>
 using namespace std;
-int cnt, K, out;
 
-void merge(vector<int>& v, int p, int q, int r) 
-{
-	vector<int> tmp(r - p + 1);
-	int i = p, j = q + 1, t = 0;
-	while (i <= q && j <= r) {
-		if (v[i] <= v[j]) {
-			tmp[t++] = v[i++];
-		}
-		else {
-			tmp[t++] = v[j++];
-		}
-	}
-	while (i <= q)
-		tmp[t++] = v[i++];
-	while (j <= r)
-		tmp[t++] = v[j++];
-	i = p, t = 0;
-	while (i <= r) {
-		v[i++] = tmp[t++];
-		if (++cnt == K) {
-			out = v[i - 1];
-		}
-	}
-}
+void bt(int N, int M, int d) {
+	static int a[8];
+	static int idx;
 
-void merge_sort(vector<int>& v, int p, int r)
-{
-	if (p < r) {
-		int q = (p + r) / 2;
-		merge_sort(v, p, q);
-		merge_sort(v, q + 1, r);
-		merge(v, p, q, r);
+	if (d == M) {
+		for (int i = 0; i < d; i++) {
+			cout << a[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int n = 1; n <= N; n++) {
+		a[idx++] = n;
+		bt(N, M, d + 1);
+		idx--;
 	}
 }
 
 int main()
 {
-	cin.tie(0);
-
-	int N; cin >> N >> K;
-	vector<int> v(N);
-	for (int i = 0; i < N; i++)
-		cin >> v[i];
-	merge_sort(v, 0, N - 1);
-	if (K > cnt) out = -1;
-	cout << out;
+	int N, M;
+	cin >> N >> M;
+	bt(N, M, 0);
 }
