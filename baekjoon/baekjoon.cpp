3b49f9d3a4f4ad0ad8ae14c422b8af4b950fd950
lja3723 <lja3723@gmail.com>
2022-10-18 14:00:55 +0900

    solve 24060

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,21 +1,53 @@
-﻿//problem No. 11659, 구간 합 구하기 4
+﻿//problem No. 24060, 알고리즘 수업 - 병합 정렬 1
 #include <iostream>
 #include <vector>
 using namespace std;
+int cnt, K, out;
+
+void merge(vector<int>& v, int p, int q, int r) 
+{
+	vector<int> tmp(r - p + 1);
+	int i = p, j = q + 1, t = 0;
+	while (i <= q && j <= r) {
+		if (v[i] <= v[j]) {
+			tmp[t++] = v[i++];
+		}
+		else {
+			tmp[t++] = v[j++];
+		}
+	}
+	while (i <= q)
+		tmp[t++] = v[i++];
+	while (j <= r)
+		tmp[t++] = v[j++];
+	i = p, t = 0;
+	while (i <= r) {
+		v[i++] = tmp[t++];
+		if (++cnt == K) {
+			out = v[i - 1];
+		}
+	}
+}
+
+void merge_sort(vector<int>& v, int p, int r)
+{
+	if (p < r) {
+		int q = (p + r) / 2;
+		merge_sort(v, p, q);
+		merge_sort(v, q + 1, r);
+		merge(v, p, q, r);
+	}
+}
 
 int main()
 {
 	cin.tie(0);
-	int N, M; 
-	cin >> N >> M;
-	vector<int> v(N + 1);
-	vector<int> s(N + 1);
-	for (int i = 1; i <= N; i++) {
+
+	int N; cin >> N >> K;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++)
 		cin >> v[i];
-		s[i] = s[i - 1] + v[i];
-	}
-	while (M--) {
-		int i, j; cin >> i >> j;
-		cout << s[j] - s[i - 1] << "\n";
-	}
+	merge_sort(v, 0, N - 1);
+	if (K > cnt) out = -1;
+	cout << out;
 }
