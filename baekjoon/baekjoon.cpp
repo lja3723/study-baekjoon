2674298d08b51086dbb465d6b5a02597f7ba7865
lja3723 <lja3723@gmail.com>
2022-10-19 23:23:25 +0900

    cleaning

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,40 +1,8 @@
-﻿//problem No. 9663, N-Queen
+﻿//problem No. 2579, 계단 오르기
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
-
-void NQueen(int N, int& cnt, int depth) {
-	static int q[15];
-
-	if (depth < N) {
-		for (int n = 0; n < N; n++) {
-			bool able = [depth, n]() {
-				for (int j = 0; j < depth; j++) {
-					if (n == q[j] || abs(n - q[j]) == depth - j) {
-						return false;
-					}
-				}
-				return true;
-			}();
-
-			if (able) {
-				q[depth] = n;
-				NQueen(N, cnt, depth + 1);
-			}
-		}
-	}
-	else {
-		cnt++;
-	}
-
-
-}
 
 int main()
 {
-	int N, cnt = 0;
-	cin >> N;
-	NQueen(N, cnt, 0);
-	cout << cnt;
+
 }
