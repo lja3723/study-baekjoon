8f4109e3a33680d35d523c14d057ee55991ca049
lja3723 <lja3723@gmail.com>
2022-10-27 22:28:44 +0900

    solve 1463, 1로 만들기

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,15 +1,32 @@
-﻿//problem No. 2579, 계단 오르기
+﻿//problem No. 1463, 1로 만들기
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int stair[300], curr;
+int dp[1000001] = { 0, 0, 1, 1 }, N;
 
+int getMin(int N) {
+	int& r = dp[N];
+	if (N < 2)
+		return 0;
+
+	if (r == 0) {
+		r = getMin(N - 1) + 1;
+
+		if (N % 2 == 0) {
+			r = min({ r, getMin(N / 2) + 1 });
+		}
+
+		if (N % 3 == 0) {
+			r = min({ r, getMin(N / 3) + 1 });
+		}
+	}
+
+	return r;
+}
 
 int main()
 {
-	int N; cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> stair[i];
-
-
+	cin >> N;
+	cout << getMin(N);
 }
