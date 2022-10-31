51bdcf3549d50800ba06b0bf34d5145c546911ce
lja3723 <lja3723@gmail.com>
2022-10-31 11:19:12 +0900

    solve 9465, 스티커

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,32 +1,34 @@
-﻿//problem No. 1463, 1로 만들기
+﻿//problem No. 9465, 스티커
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#define MAX 100001
 using namespace std;
 
-int dp[1000001] = { 0, 0, 1, 1 }, N;
-
-int getMin(int N) {
-	int& r = dp[N];
-	if (N < 2)
-		return 0;
-
-	if (r == 0) {
-		r = getMin(N - 1) + 1;
-
-		if (N % 2 == 0) {
-			r = min({ r, getMin(N / 2) + 1 });
-		}
-
-		if (N % 3 == 0) {
-			r = min({ r, getMin(N / 3) + 1 });
-		}
-	}
-
-	return r;
-}
+int t, n;
+int score[2][MAX];
+int dp[2][MAX];
 
 int main()
 {
-	cin >> N;
-	cout << getMin(N);
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		for (int i = 0; i < 2; i++)
+			for (int j = 1; j <= n; j++)
+				cin >> score[i][j];
+		
+		dp[0][0] = dp[1][0] = 0;
+		dp[0][1] = score[0][1];
+		dp[1][1] = score[1][1];
+		
+		for (int i = 2; i <= n; i++) {
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + score[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + score[1][i];
+		}
+
+		cout << max(dp[0][n], dp[1][n]) << "\n";
+	}
 }
