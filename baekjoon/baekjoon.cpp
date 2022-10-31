f0636790a14321bf8af17b45f98988889de6dfb2
lja3723 <lja3723@gmail.com>
2022-10-31 11:19:12 +0900

    solve 11052, 카드 구매하기

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,32 +1,28 @@
-﻿//problem No. 1463, 1로 만들기
+﻿//problem No. 11052, 카드 구매하기
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int dp[1000001] = { 0, 0, 1, 1 }, N;
-
-int getMin(int N) {
-	int& r = dp[N];
-	if (N < 2)
-		return 0;
-
-	if (r == 0) {
-		r = getMin(N - 1) + 1;
-
-		if (N % 2 == 0) {
-			r = min({ r, getMin(N / 2) + 1 });
-		}
-
-		if (N % 3 == 0) {
-			r = min({ r, getMin(N / 3) + 1 });
-		}
-	}
-
-	return r;
-}
+int N;
+int p[10001], dp[10001];
 
 int main()
 {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+
 	cin >> N;
-	cout << getMin(N);
+	for (int i = 1; i <= N; i++) {
+		cin >> p[i];
+	}
+
+	dp[1] = p[1];
+	for (int n = 2; n <= N; n++) {
+		dp[n] = p[n];
+		for (int i = 1; i <= N - 1; i++) {
+			dp[n] = max(dp[n], dp[i] + p[n - i]);
+		}
+	}
+
+	cout << dp[N];
 }
