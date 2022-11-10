da31ff4bb2f9d8f0ec04828003d3bc3436b021c8
lja3723 <lja3723@gmail.com>
2022-11-10 22:30:42 +0900

    solve 15829, Hashing

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,8 +1,26 @@
-﻿//problem No. 15686, 치킨 배달
+﻿//problem No. 15829, Hashing
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
+	int L;
+	cin >> L;
+
+	string s;
+	cin >> s;
+
+	long long H = 0, p1 = 31, p2 = 1234567891;
+	for (int i = 0; i < s.size(); i++) {
+		long long a = s[i] - 'a' + 1;
+		int j = i;
+		while (j--) {
+			a *= p1;
+			a %= p2;
+		}
+		H += a;
+		H %= p2;
+	}
+
+	cout << H % p2;
 }
