8e3247e1a863ed879f2d3184eaa88d02d1182d4d
lja3723 <lja3723@gmail.com>
2022-11-14 19:19:18 +0900

    solve 2667, 단지번호붙이기

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,44 +1,61 @@
-﻿//problem No. 2606, 바이러스
+﻿//problem No. 2667, 단지번호붙이기
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <algorithm>
 using namespace std;
 
-int N, M, ans;
-vector<vector<int>> graph(101);
-vector<int> visited(101);
+int N;
+string map[25];
+vector<int> total;
 
-void bfs() {
-	queue<int> q;	
-	q.push(1);
-	visited[1]++;
+void bfs(int Y, int X) {
+	if (map[Y][X] == '0') return;
+	map[Y][X] = '0';
+	
+	queue<int> y, x;
+	y.push(Y);
+	x.push(X);
 
-	while (!q.empty()) {
-		int v = q.front();
-		q.pop();
-		ans++;
+	int num = 0;
+	while (!y.empty()) {
+		int yy = y.front(), xx = x.front();
+		y.pop(), x.pop();
+		num++;
 
-		for (int nv : graph[v])
-			if (!visited[nv]) {
-				visited[nv]++;
-				q.push(nv);
-			}
+		int dy[4] = { 1, -1, 0, 0 };
+		int dx[4] = { 0, 0, 1, -1 };
+		for (int i = 0; i < 4; i++) {
+			int cy = yy + dy[i];
+			int cx = xx + dx[i];
+
+			if (!(0 <= cy && cy < N) || !(0 <= cx && cx < N) || map[cy][cx] == '0') 
+				continue;
+
+			map[cy][cx] = '0';
+			y.push(cy), x.push(cx);
+		}
 	}
+
+	total.push_back(num);
 }
 
 int main()
 {
-	cin >> N >> M;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> map[i];
+
+	for (int y = 0; y < N; y++) 
+		for (int x = 0; x < N; x++)
+			bfs(y, x);
 	
-	for (int i = 0; i < M; i++) {
-		int v, u;
-		cin >> v >> u;
-		graph[v].push_back(u);
-		graph[u].push_back(v);
+	sort(total.begin(), total.end());
+
+	cout << total.size() << "\n";
+
+	for (int ans : total) {
+		cout << ans << "\n";
 	}
-
-	bfs();
-
-	cout << --ans;
 }
