844ffecf09dbc3ff9784c3ec0b7e1ee53073cdef
lja3723 <lja3723@gmail.com>
2022-11-14 22:02:20 +0900

    solve 1697, 숨바꼭질

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,61 +1,44 @@
-﻿//problem No. 2667, 단지번호붙이기
+﻿//problem No. 1697, 숨바꼭질
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
-int N;
-string map[25];
-vector<int> total;
+struct PT { int p, t; }; //위치 & 시간
+int visited[100001];
+int n, k, ans;
 
-void bfs(int Y, int X) {
-	if (map[Y][X] == '0') return;
-	map[Y][X] = '0';
-	
-	queue<int> y, x;
-	y.push(Y);
-	x.push(X);
+void bfs() {
+	if (n == k) return;
 
-	int num = 0;
-	while (!y.empty()) {
-		int yy = y.front(), xx = x.front();
-		y.pop(), x.pop();
-		num++;
+	queue<PT> q;
+	q.push({ n, 0 });
+	visited[n]++;
 
-		int dy[4] = { 1, -1, 0, 0 };
-		int dx[4] = { 0, 0, 1, -1 };
-		for (int i = 0; i < 4; i++) {
-			int cy = yy + dy[i];
-			int cx = xx + dx[i];
+	while (true) {
+		PT now = q.front();
+		q.pop();
 
-			if (!(0 <= cy && cy < N) || !(0 <= cx && cx < N) || map[cy][cx] == '0') 
+		int i, nt = now.t + 1;
+		int np[3] = { now.p + 1, now.p - 1, 2 * now.p };
+		for (i = 0; i < 3; i++) {
+			if (!(0 <= np[i] && np[i] <= 100000) || visited[np[i]])
 				continue;
 
-			map[cy][cx] = '0';
-			y.push(cy), x.push(cx);
+			if (np[i] == k) {
+				ans = nt;
+				return;
+			}
+
+			q.push({ np[i], nt });
+			visited[np[i]]++;
 		}
 	}
-
-	total.push_back(num);
 }
 
 int main()
 {
-	cin >> N;
-
-	for (int i = 0; i < N; i++)
-		cin >> map[i];
-
-	for (int y = 0; y < N; y++) 
-		for (int x = 0; x < N; x++)
-			bfs(y, x);
-	
-	sort(total.begin(), total.end());
-
-	cout << total.size() << "\n";
-
-	for (int ans : total) {
-		cout << ans << "\n";
-	}
+	cin >> n >> k;
+	bfs();
+	cout << ans;
 }
