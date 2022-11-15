2c4e3182e7b43318007698fc17d599194786f980
lja3723 <lja3723@gmail.com>
2022-11-15 20:27:43 +0900

    solve 10844, 쉬운 계단수

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,21 +1,29 @@
-﻿//problem No. 1912, 연속합
+﻿//problem No. 10844, 쉬운 계단수
 #include <iostream>
 using namespace std;
 
-int dp[100001];
+int dp[101][10];
 
 int main() {
-	int n;
-	cin >> n;
+	int N;
+	cin >> N;
 
-	//dp[0] = -1000;
+	for (int i = 1; i < 10; i++)
+		dp[1][i]++;
 
-	int ans = -1e9;
-	for (int dn = 1; dn <= n; dn++) {
-		cin >> dp[dn];
-		dp[dn] = max(dp[dn - 1] + dp[dn], dp[dn]);
-		ans = max(ans, dp[dn]);
+	int p = 1e9;
+	for (int n = 2; n <= N; n++) {
+		dp[n][0] = dp[n - 1][1] % p;
+
+		for (int j = 1; j <= 8; j++)
+			dp[n][j] = (dp[n - 1][j - 1] + dp[n - 1][j + 1]) % p;
+
+		dp[n][9] = dp[n - 1][8] % p;
 	}
+
+	int ans = 0;
+	for (int i = 0; i < 10; i++)
+		ans = (ans + dp[N][i]) % p;
 
 	cout << ans;
 }
