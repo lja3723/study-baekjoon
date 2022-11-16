d9d00996f1e7f76faa24614f2777eadf469a1473
lja3723 <lja3723@gmail.com>
2022-11-16 19:54:20 +0900

    solve 1010, 다리 놓기

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,51 +1,33 @@
-﻿//problem No. 7569, 토마토
+﻿//problem No. 1010, 다리 놓기
 #include <iostream>
-#include <vector>
-#include <queue>
 using namespace std;
 
-struct HIJT { int h, i, j, t; }; //좌표(h,i,j) 시간t
+int combi[30][30];
 
-int map[100][100][100], h, i, j, N, M, H;
-queue<HIJT> q;
+int getCombi(int M, int N) {
+	if (combi[M][N] == 0) {
+		combi[M][M] = getCombi(M - 1, N - 1) + getCombi(M - 1, N);
+	}
+
+	return combi[M][N];
+}
 
 int main() {
-	cin >> M >> N >> H;
+	ios::sync_with_stdio(0), cin.tie(0);
+	int T, N, M;
 
-	int tCnt = 0; //처음 안익은 토마토 수
-	for (h = 0; h < H; h++)
-		for (i = 0; i < N; i++)
-			for (j = 0; j < M; j++) {
-				cin >> map[h][i][j];
-				if (map[h][i][j] == 1)
-					q.push({ h, i, j, 0 });
-				else if (map[h][i][j] == 0)
-					tCnt++;
-			}
-
-	int ans = 0;
-	int rtCnt = 0; //시간이 지나 익은 토마토 수
-	while (!q.empty()) {
-		HIJT cur = q.front(); q.pop();
-		ans = cur.t;
-
-		int dh[] = { -1, 1, 0, 0, 0, 0 };
-		int di[] = { 0, 0, 0, 0, -1, 1 };
-		int dj[] = { 0, 0, -1, 1, 0, 0 };
-		for (i = 0; i < 6; i++) {
-			int nh = cur.h + dh[i];
-			int ni = cur.i + di[i];
-			int nj = cur.j + dj[i];
-			if (!(0 <= nh && nh < H) || !(0 <= ni && ni < N) || !(0 <= nj && nj < M))
-				continue;
-			if (map[nh][ni][nj] == -1 || map[nh][ni][nj] == 1)
-				continue;
-
-			map[nh][ni][nj] = 1;
-			rtCnt++;
-			q.push({ nh, ni, nj, cur.t + 1 });
+	for (M = 1; M < 30; M++) {
+		for (N = 0; N <= M; N++) {
+			if (0 == N || M == N)
+				combi[M][N] = 1;
+			else
+				combi[M][N] = combi[M - 1][N - 1] + combi[M - 1][N];
 		}
 	}
 
-	cout << (tCnt == rtCnt ? ans : -1);
+	cin >> T;
+	while (T--) {
+		cin >> N >> M;
+		cout << getCombi(M, N) << "\n";
+	}
 }
