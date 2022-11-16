ae7922608d08a486c9f06665b1a38fed120503ce
lja3723 <lja3723@gmail.com>
2022-11-16 22:23:07 +0900

    solve 10026, 적록색약

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,47 +1,62 @@
-﻿//problem No. 4963, 섬의 개수
+﻿//problem No. 10026, 적록색약
 #include <iostream>
 using namespace std;
 
-int map[50][50];
-int h, w, i, j, cnt;
+int N, i, j;
+string paint[100];
+int visited[100][100];
 
-void dfs(int i, int j) {
-	if (map[i][j] == 0) return;
-	map[i][j] = 0;
+void dfs(int i, int j, char color, int mode) {
+	if (visited[i][j]) return;
+	visited[i][j] = 1;
 
-	int di[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
-	int dj[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
-	for (int d = 0; d < 8; d++) {
+	int di[] = { 1, -1, 0, 0 };
+	int dj[] = { 0, 0, 1, -1 };
+	for (int d = 0; d < 4; d++) {
 		int ni = i + di[d];
 		int nj = j + dj[d];
-		if (!(0 <= ni && ni < h) || !(0 <= nj && nj < w))
+
+		if (!(0 <= ni && ni < N) || !(0 <= nj && nj < N)) 
 			continue;
-		if (map[ni][nj] == 0)
+		if (visited[ni][nj])
 			continue;
 
-		dfs(ni, nj);
+		if (mode == 0) {
+			if (color != paint[ni][nj])
+				continue;
+		}
+
+		if (mode == 1) {
+			if (color == 'B' && paint[ni][nj] != 'B')
+				continue;
+			if (color != 'B' && paint[ni][nj] == 'B')
+				continue;				
+		}
+
+		dfs(ni, nj, color, mode);
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(0), cin.tie(0);
-	while (1) {
-		cin >> w >> h;
-		if (w == 0 && h == 0) break;
+	cin >> N;
 
-		for (i = 0; i < h; i++)
-			for (j = 0; j < w; j++)
-				cin >> map[i][j];
+	for (i = 0; i < N; i++)
+		cin >> paint[i];
+
+	int cnt, mode = -1;
+	while (++mode < 2) {
+		if (mode == 1)
+			for (i = 0; i < N; i++)
+				fill(visited[i], visited[i] + N, 0);
 
 		cnt = 0;
-		for (i = 0; i < h; i++)
-			for (j = 0; j < w; j++) {
-				if (map[i][j] == 1) {
+		for (i = 0; i < N; i++)
+			for (j = 0; j < N; j++)
+				if (!visited[i][j]) {
 					cnt++;
-					dfs(i, j);
+					dfs(i, j, paint[i][j], mode);
 				}
-			}
 
-		cout << cnt << "\n";
+		cout << cnt << " ";
 	}
 }
