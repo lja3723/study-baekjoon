6cf1f06cc60df814d8dfb14aca4784e8ad45c226
lja3723 <lja3723@gmail.com>
2022-11-17 13:17:06 +0900

    solve 1541, 잃어버린 괄호

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,55 +1,31 @@
-﻿//problem No. 7562, 나이트의 이동
+﻿//problem No. 1541, 잃어버린 괄호
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <string>
+#include <sstream>
 using namespace std;
 
-struct IJ { int i, j, mv; };
-
-int map[300][300];
-int I, T, i, j;
-
-void bfs() {
-	queue<IJ> q;
-	cin >> i >> j;
-	q.push({ i, j, 0 });
-	map[i][j] = 1;
-
-	cin >> i >> j;
-	IJ goal{ i, j, 0 };
-
-	int ans = 0;
-	while (!q.empty()) {
-		IJ cur = q.front(); q.pop();
-
-		if (cur.i == goal.i && cur.j == goal.j) {
-			cout << cur.mv << "\n";
-			return;
-		}
-
-		int di[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-		int dj[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-		for (int d = 0; d < 8; d++) {
-			int ni = cur.i + di[d];
-			int nj = cur.j + dj[d];
-
-			if (!(0 <= ni && ni < I) || !(0 <= nj && nj < I))
-				continue;
-			if (map[ni][nj])
-				continue;
-
-			q.push({ ni, nj, cur.mv + 1 });
-			map[ni][nj] = 1;
-		}
-	}
-}
-
 int main() {
-	cin >> T;
-	while (T--) {
-		cin >> I;
-		for (i = 0; i < I; i++)
-			fill(map[i], map[i] + I, 0);
-		bfs();
+	string input, p;
+	cin >> input;
+
+	vector<string> sv1;
+	stringstream ss1(input);
+	while (getline(ss1, p, '-')) //{
+		//cout << p << "\n";
+		sv1.push_back(p);
+	//}
+
+	//cout << "first clear\n\n";
+
+	int sum = 0;
+	for (int i = 0; i < sv1.size(); i++) {
+		stringstream ss2(sv1[i]);
+		while (getline(ss2, p, '+')) //{
+			//cout << p << "\n";
+			sum += i == 0 ? stoi(p) : -stoi(p);
+		//}
 	}
+
+	cout << sum;
 }
