48674a09f5e03ad160401fd730c4e5ba9baaa4c6
lja3723 <lja3723@gmail.com>
2022-11-18 23:07:35 +0900

    solve 13305, 주유소

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,53 +1,26 @@
-﻿//problem No. 12865, 평범한 배낭
+﻿//problem No. 13305, 주유소
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int R, C, ans = 1;
-string map[20];
-int visited[20][20];
-int showed[26];
-
-void dfs(int i, int j, int len) {
-	if (visited[i][j]) return;
-	visited[i][j] = 1;
-	showed[map[i][j] - 'A'] = 1;
-
-	int di[] = { 1, -1, 0, 0 };
-	int dj[] = { 0, 0, 1, -1 };
-
-	int visit = 0;
-	for (int d = 0; d < 4; d++) {
-		int ni = i + di[d];
-		int nj = j + dj[d];
-
-		if (!(0 <= ni && ni < R) || !(0 <= nj && nj < C))
-			continue;
-		if (visited[ni][nj])
-			continue;
-		if (showed[map[ni][nj] - 'A']) {
-			//visited[ni][nj] = 1;
-			continue;
-		}
-
-		showed[map[ni][nj] - 'A'] = 1;
-		dfs(ni, nj, len + 1);
-		showed[map[ni][nj] - 'A'] = 0;	
-		visit++;
-	}
-
-	visited[i][j] = 0;
-	if (!visit) {
-		ans = max(ans, len);
-	}
-}
+int N, city[100000], road[100000];
 
 int main() {
-	cin >> R >> C;
-	for (int i = 0; i < R; i++)
-		cin >> map[i];
+	cin >> N;
+	for (int i = 0; i < N - 1; i++)
+		cin >> road[i];
+	for (int i = 0; i < N; i++) 
+		cin >> city[i];
+	
+	long long dist = 0, cost = 0, minCity = city[0];
+	for (int i = 1; i < N; i++) {
+		dist += road[i - 1];
+		if (minCity > city[i]) {
+			cost += minCity * dist;
+			minCity = city[i];
+			dist = 0;
+		}
+	}
+	cost += minCity * dist;
 
-	dfs(0, 0, 1);
-
-	cout << ans;
+	cout << cost;
 }
