1571f53f38dedf4b71e8c8a5f4874d8d6ad3794d
lja3723 <lja3723@gmail.com>
2022-11-18 22:32:05 +0900

    solve 12865, 평범한 배낭

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -3,22 +3,51 @@
 #include <vector>
 using namespace std;
 
-int N, K;
-int obj[101][2];
-int dp[101][100001]; //dp[n][k] 무게 k 이내에 n개의 보석을 골랐을 때 이익의 최대값
+int R, C, ans = 1;
+string map[20];
+int visited[20][20];
+int showed[26];
 
-int main() {
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++) {
-		cin >> obj[i][0] >> obj[i][1]; //w, v;
+void dfs(int i, int j, int len) {
+	if (visited[i][j]) return;
+	visited[i][j] = 1;
+	showed[map[i][j] - 'A'] = 1;
+
+	int di[] = { 1, -1, 0, 0 };
+	int dj[] = { 0, 0, 1, -1 };
+
+	int visit = 0;
+	for (int d = 0; d < 4; d++) {
+		int ni = i + di[d];
+		int nj = j + dj[d];
+
+		if (!(0 <= ni && ni < R) || !(0 <= nj && nj < C))
+			continue;
+		if (visited[ni][nj])
+			continue;
+		if (showed[map[ni][nj] - 'A']) {
+			//visited[ni][nj] = 1;
+			continue;
+		}
+
+		showed[map[ni][nj] - 'A'] = 1;
+		dfs(ni, nj, len + 1);
+		showed[map[ni][nj] - 'A'] = 0;	
+		visit++;
 	}
 
-	for (int n = 1; n <= N; n++)
-		for (int k = 1; k <= K; k++)
-			if (k < obj[n][0])
-				dp[n][k] = dp[n - 1][k];
-			else
-				dp[n][k] = max(obj[n][1] + dp[n - 1][k - obj[n][0]], dp[n - 1][k]);
+	visited[i][j] = 0;
+	if (!visit) {
+		ans = max(ans, len);
+	}
+}
 
-	cout << dp[N][K];
+int main() {
+	cin >> R >> C;
+	for (int i = 0; i < R; i++)
+		cin >> map[i];
+
+	dfs(0, 0, 1);
+
+	cout << ans;
 }
