6cad68d76d53addef9e70d30d5d0f973dd0e3d34
lja3723 <lja3723@gmail.com>
2022-11-20 21:44:53 +0900

    re solve 1915, 가장 큰 정사각형

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -14,21 +14,13 @@ int main() {
 		for (j = 0; j < M; j++)
 			map[i][j] = s[j] - '0';
 	}
+
 	int ans = 0;
-
-	for (i = 0; i < N; i++) {
-		dp[i][0] = map[i][0];
-		ans = max(ans, dp[i][0]);
-	}
-
-	for (j = 1; j < M; j++) {
-		dp[0][j] = map[0][j];
-		ans = max(ans, dp[0][j]);
-	}
-
 	for (i = 1; i < N; i++) {
 		for (j = 1; j < M; j++) {
-			if (map[i][j] == 0)
+			if (i == 0 || j == 0)
+				dp[i][j] = map[i][j];
+			else if (map[i][j] == 0)
 				dp[i][j] = 0;
 			else 
 				dp[i][j] = min({ dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1] }) + 1;
