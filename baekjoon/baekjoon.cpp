5fb51b6d076abb9e74169839812caedfe00c33cc
lja3723 <lja3723@gmail.com>
2022-11-21 22:52:35 +0900

    solve 9251, LCS

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,47 +1,19 @@
-﻿//problem No. 2193, 이친수
+﻿//problem 9251, LCS
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int n;
-long long dp[91][2];
-
-/*
- \  0  1  2  3  4  5  6  7  8
-0 | 0  0  1  
-1 | 0  1  0  
-
-
-10 0
-
-10 1
-
-100 0
-101 0
-
-100 1
-
-
-1000 0
-1001 0
-1010 0
-
-1000 1
-
-*/
+int dp[1001][1001];
 
 int main() {
-	cin >> n;
+	string a, b;
+	cin >> a >> b;
 
-	dp[1][0] = 0;
-	dp[1][1] = 1;
-	dp[2][0] = 1;
-	dp[2][1] = 0;
+	for (int i = 1; i <= a.size(); i++) 
+		for (int j = 1; j <= b.size(); j++) 
+			if (a[i - 1] == b[j - 1]) 
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 
-	for (int i = 3; i <= n; i++) {
-		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-		dp[i][1] = dp[i - 1][0];
-	}
-
-	cout << dp[n][0] + dp[n][1];
+	cout << dp[a.size()][b.size()];
 }
