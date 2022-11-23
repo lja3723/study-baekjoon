13cb5e29377af2d08fdf9552360abf60e64c6ebe
lja3723 <lja3723@gmail.com>
2022-11-23 11:35:41 +0900

    solve 1520, 내리막 길

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,36 +1,42 @@
-﻿//problem 11054, 가장 긴 바이토닉 부분 수열
+﻿//problem 1520, 내리막 길
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int dat[1001], N, i, j, LIS[1001], LDS[1001];
+int M, N, i, j;
+int map[501][501], dp[501][501];
+
+int getPathCnt(int m, int n) {
+	if (dp[m][n] != -1)
+		return dp[m][n];
+
+	dp[m][n] = 0;
+
+	int di[] = { 1, -1, 0, 0 };
+	int dj[] = { 0, 0, 1, -1 };
+	for (int d = 0; d < 4; d++) {
+		int ni = m + di[d];
+		int nj = n + dj[d];
+
+		if (!(0 < ni && ni <= M) || !(0 < nj && nj <= N))
+			continue;
+		if (map[ni][nj] <= map[m][n])
+			continue;
+
+		dp[m][n] += getPathCnt(ni, nj);
+	}
+
+	return dp[m][n];
+}
 
 int main() {
-	cin >> N;
+	cin >> M >> N;
+	for (i = 1; i <= M; i++)
+		for (j = 1; j <= N; j++) {
+			cin >> map[i][j];
+			dp[i][j] = -1;
+		}
 
-	for (i = 1; i <= N; i++) cin >> dat[i];
+	dp[1][1] = 1;
+	cout << getPathCnt(M, N);
 
-	//i가 끝점인 LIS 구하기
-	for (i = 1; i <= N; i++) {
-		LIS[i] = 1;
-		for (j = 1; j < i; j++)
-			if (dat[j] < dat[i])
-				LIS[i] = max(LIS[i], LIS[j] + 1);
-	}
-
-	//i가 시작점인 LDS 구하기
-	for (i = N; i >= 1; i--) {
-		LDS[i] = 1;
-		for (j = N; i < j; j--)
-			if (dat[i] > dat[j])
-				LDS[i] = max(LDS[i], LDS[j] + 1);
-	}
-
-	//i가 최대값인 LBS 구하기
-	int ans = 0;
-	for (i = 1; i <= N; i++)
-		ans = max(ans, LIS[i] + LDS[i] - 1);
-
-	cout << ans;
 }
