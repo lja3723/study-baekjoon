7dacd092dd3a5e3ff3dc89322d7fdb86e1851d6e
lja3723 <lja3723@gmail.com>
2022-11-23 10:53:51 +0900

    solve 11054, 가장 긴 바이토닉 부분 수열

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,23 +1,36 @@
-﻿//problem 11055, 가장 큰 증가 부분 수열
+﻿//problem 11054, 가장 긴 바이토닉 부분 수열
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int dat[1001], N, i, j, dp[1001];
+int dat[1001], N, i, j, LIS[1001], LDS[1001];
 
 int main() {
 	cin >> N;
-	
-	for (i = 1; i <= N; i++) 
-		cin >> dat[i];
 
-	int ans = 0;
+	for (i = 1; i <= N; i++) cin >> dat[i];
+
+	//i가 끝점인 LIS 구하기
 	for (i = 1; i <= N; i++) {
-		dp[i] = dat[i];
+		LIS[i] = 1;
 		for (j = 1; j < i; j++)
 			if (dat[j] < dat[i])
-				dp[i] = max(dp[i], dp[j] + dat[i]);
-		ans = max(ans, dp[i]);
+				LIS[i] = max(LIS[i], LIS[j] + 1);
 	}
+
+	//i가 시작점인 LDS 구하기
+	for (i = N; i >= 1; i--) {
+		LDS[i] = 1;
+		for (j = N; i < j; j--)
+			if (dat[i] > dat[j])
+				LDS[i] = max(LDS[i], LDS[j] + 1);
+	}
+
+	//i가 최대값인 LBS 구하기
+	int ans = 0;
+	for (i = 1; i <= N; i++)
+		ans = max(ans, LIS[i] + LDS[i] - 1);
 
 	cout << ans;
 }
