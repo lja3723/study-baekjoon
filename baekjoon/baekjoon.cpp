7daf61ce2334d1cf5a798a8ac9e143fa503b69c1
lja3723 <lja3723@gmail.com>
2022-11-27 19:06:41 +0900

    solve 1676, 팩토리얼 0의 개수

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,39 +1,23 @@
-﻿//problem 11723, 집합
+﻿//problem No. 1676, 팩토리얼 0의 개수
 #include <iostream>
 using namespace std;
 
-int M, x, S;
-string s;
-
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(0);
+	int N, d2 = 0, d5 = 0;
 
-	cin >> M;
-	while (M--) {
-		cin >> s;
-		if (!s.compare("add")) {
-			cin >> x;
-			S |= 1 << x;
+	cin >> N;
+
+	for (int n = 1; n <= N; n++) {
+		int k = n;
+		while (k % 5 == 0) {
+			k /= 5;
+			d5++;
 		}
-		else if (!s.compare("remove")) {
-			cin >> x;
-			S &= ~(1 << x);
-		}
-		else if (!s.compare("check")) {
-			cin >> x;
-			cout << !!(1 << x & S) << "\n";
-		}
-		else if (!s.compare("toggle")) {
-			cin >> x;
-			S ^= 1 << x;
-		}
-		else if (!s.compare("all")) {
-			S = ~0;
-		}
-		else if (!s.compare("empty")) {
-			S = 0;
+		while (k % 2 == 0) {
+			k /= 2;
+			d2++;
 		}
 	}
+
+	cout << min(d2, d5);
 }
