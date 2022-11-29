6687a2502090eeb2a13811460026311bde19ee0e
lja3723 <lja3723@gmail.com>
2022-11-29 12:01:44 +0900

    solve 9935, 문자열 폭발

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,18 +1,37 @@
-﻿//problem No. 2559, 수열
+﻿//problem No. 9935, 문자열 폭발
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int N, K, i, ans = -1e9, dat[100001];
+string str, bomb, st;
+int i, j;
 
 int main() {
-	cin >> N >> K;
-	for (i = 1; i <= N; i++) {
-		cin >> dat[i];
-		dat[i] += dat[i - 1];
+	cin >> str >> bomb;
+	int szB = bomb.size();	
+
+	for (i = str.size() - szB; i >= 0; i--) {
+		if (str.size() < i + szB)
+			continue;
+		else if (str.substr(i, szB) == bomb) {
+			for (j = 0; j < szB; j++)
+				str.pop_back();
+
+			for (j = 0; !st.empty() && j < szB - 1; j++) {
+				str.push_back(st.back());
+				st.pop_back();
+			}
+		}
+		else {
+			st.push_back(str.back());
+			str.pop_back();
+		}		
 	}
 
-	for (i = K; i <= N; i++) 
-		ans = max(ans, dat[i] - dat[i - K]);
-	
-	cout << ans;
+	while (!st.empty()) {
+		str.push_back(st.back());
+		st.pop_back();
+	}
+
+	cout << (str.empty() ? "FRULA" : str);
 }
