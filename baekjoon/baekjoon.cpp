f1e47e7d263f74c7df5bc19b36333ad492af66a8
lja3723 <lja3723@gmail.com>
2022-11-30 13:38:57 +0900

    solve 1043, 거짓말

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,37 +1,60 @@
-﻿//problem No. 9935, 문자열 폭발
+﻿//problem No. 1043, 거짓말
 #include <iostream>
 #include <vector>
 using namespace std;
 
-string str, bomb, st;
-int i, j;
+int N, M, i, j, party[50][51], visited[51], know;
+vector<int> knows, g[51];
+
+/*
+반례:
+9 4
+1 1
+4 1 2 3 4
+4 5 6 7 8
+2 8 9
+2 4 9
+*/
+void dfs(int v) {
+	if (visited[v]) return;
+	visited[v] = 1;
+
+	for (int u : g[v]) {
+		dfs(u);
+	}
+}
 
 int main() {
-	cin >> str >> bomb;
-	int szB = bomb.size();	
+	cin >> N >> M >> know;
+	for (i = 0; i < know; i++) {
+		cin >> j;
+		knows.push_back(j);
+	}
 
-	for (i = str.size() - szB; i >= 0; i--) {
-		if (str.size() < i + szB)
-			continue;
-		else if (str.substr(i, szB) == bomb) {
-			for (j = 0; j < szB; j++)
-				str.pop_back();
+	for (i = 0; i < M; i++) {
+		cin >> party[i][0]; //파티 참여인원
 
-			for (j = 0; !st.empty() && j < szB - 1; j++) {
-				str.push_back(st.back());
-				st.pop_back();
+		for (j = 1; j <= party[i][0]; j++) {
+			cin >> party[i][j];
+			if (j > 1) { //파티 참여인원끼리의 관계생성
+				g[party[i][j - 1]].push_back(party[i][j]);
+				g[party[i][j]].push_back(party[i][j - 1]);
 			}
 		}
-		else {
-			st.push_back(str.back());
-			str.pop_back();
-		}		
 	}
 
-	while (!st.empty()) {
-		str.push_back(st.back());
-		st.pop_back();
+	for (int v : knows) dfs(v);
+
+	int ans = 0;
+	for (i = 0; i < M; i++) {
+		know = 0;
+		for (j = 1; !know && j <= party[i][0]; j++)
+			if (visited[party[i][j]])
+				know = 1;
+
+		if (!know) //진실을 아는 사람이 없는 파티
+			ans++;
 	}
 
-	cout << (str.empty() ? "FRULA" : str);
+	cout << ans;
 }
