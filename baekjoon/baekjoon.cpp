b06c1dc7717fe0128566c8b1b82518b767b59614
lja3723 <lja3723@gmail.com>
2022-12-02 12:40:03 +0900

    solve 1238, 파티

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,4 +1,4 @@
-﻿//problem No. 1753, 최단경로
+﻿//problem No. 1238, 파티
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,15 +6,18 @@
 
 using namespace std;
 typedef pair<int, int> pii;
-const int MAX = 20001, INF = 2e9;
+const int MAX = 1001, INF = 2e9;
 
-int V, E, K, i;
 vector<pii> g[MAX];
-int visited[MAX], dist[MAX];
+int dist[MAX], visited[MAX];
+int toX[MAX], toHome[MAX]; //X, X에서 집으로 가는데 걸리는 시간
+int N, M, X, ans;
 
+//from에서 to로 가는 최단거리를 구한 경우, 바로 종료
+//to가 0이면 모든 최단거리 구함
 void dijkstra(int from, int to) {
-	fill(visited + 1, visited + V + 1, 0);
-	fill(dist + 1, dist + V + 1, INF);
+	fill(visited, visited + MAX, 0);
+	fill(dist, dist + MAX, INF);
 	dist[from] = 0;
 
 	priority_queue<pii, vector<pii>, greater<pii>> pq;
@@ -26,32 +29,41 @@ void dijkstra(int from, int to) {
 		if (visited[curV]) continue;
 		visited[curV] = 1;
 
-		for (i = 0; i < g[curV].size(); i++) {
-			int u = g[curV][i].first;
-			int uCost = g[curV][i].second;
+		for (auto& e : g[curV]) {
+			int u = e.first, uCost = e.second;
+
 			if (dist[u] > dist[curV] + uCost) {
 				dist[u] = dist[curV] + uCost;
 				pq.push({ dist[u], u });
 			}
 		}
 
-		if (curV == to) break;
+		if (curV == to)
+			return;
 	}
 }
 
 int main() {
-	cin >> V >> E >> K;
-	for (i = 0; i < E; i++) {
-		int u, v, w;
-		cin >> u >> v >> w;
-		g[u].push_back({ v, w });
+	cin >> N >> M >> X;
+	for (int i = 0; i < M; i++) {
+		int v, u, d;
+		cin >> v >> u >> d;
+		g[v].push_back({ u, d });
 	}
 
-	dijkstra(K, 0);
+	//X에서 각 집으로 가는 최단경로 구하기
+	dijkstra(X, 0);
+	for (int i = 1; i <= N; i++)
+		toHome[i] = dist[i];
 
-	for (i = 1; i <= V; i++)
-		if (dist[i] == INF)
-			cout << "INF\n";
-		else
-			cout << dist[i] << "\n";
+	//각 집에서 X로 가는 최단경로 구하기
+	for (int v = 1; v <= N; v++) {
+		dijkstra(v, X);
+		toX[v] = dist[X];
+	}
+
+	ans = toX[1] + toHome[1];
+	for (int v = 2; v <= N; v++)
+		ans = max(ans, toX[v] + toHome[v]);
+	cout << ans;
 }
