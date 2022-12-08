f1d1f749fa8a43db826819893b651776af079461
lja3723 <lja3723@gmail.com>
2022-12-08 14:54:37 +0900

    solve 2580, 스도쿠

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,50 +1,53 @@
-﻿//problem No. 1967, 트리의 지름
+﻿//problem No. 2580, 스도쿠
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
 using namespace std;
-#define MAX 10001
 
-int V;
-vector<pair<int, int>> g[MAX];
-int vt[MAX]; //거리저장
+struct RC { int r, c; };
+int map[9][9], rowCheck[9][10], colCheck[9][10], boxCheck[9][10];
+vector<RC> blank;
 
-void bfs(int v) {
-	for (int i = 0; i < MAX; i++)
-		vt[i] = -1;
+int boxIdx(int i, int j) {
+	return 3 * (i / 3) + (j / 3);
+}
 
-	queue<int> q;
-	vt[v] = 0;
-	q.push(v);
+void checkExist(int i, int j, int n, int flag) {
+	rowCheck[i][n] += flag;
+	colCheck[j][n] += flag;
+	boxCheck[boxIdx(i, j)][n] += flag;
+}
 
-	while (!q.empty()) {
-		int cv = q.front();
-		q.pop();
+void bt(int filled) {
+	if (blank.empty()) return;
+	if (filled == blank.size()) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++)
+				cout << map[i][j] << " ";
+			cout << "\n";
+		}
+		exit(0);
+	}
 
-		for (auto& e : g[cv]) {
-			int u = e.first;
-			int d = e.second;
-
-			if (vt[u] == -1) {
-				vt[u] = vt[cv] + d;
-				q.push(u);
-			}
+	RC cur = blank[filled];
+	for (int n = 1; n <= 9; n++) {
+		if (!rowCheck[cur.r][n] && !colCheck[cur.c][n] && !boxCheck[boxIdx(cur.r, cur.c)][n]) {
+			map[cur.r][cur.c] = n;
+			checkExist(cur.r, cur.c, n, 1);
+			bt(filled + 1);
+			map[cur.r][cur.c] = 0;
+			checkExist(cur.r, cur.c, n, -1);
 		}
 	}
 }
 
 int main() {
-	cin >> V;
+	for (int i = 0; i < 9; i++) 
+		for (int j = 0; j < 9; j++) {
+			cin >> map[i][j];
+			checkExist(i, j, map[i][j], 1);
+			if (map[i][j] == 0)
+				blank.push_back({ i, j });
+		}
 
-	for (int i = 0; i < V - 1; i++) {
-		int v, u, d;
-		cin >> v >> u >> d;
-		g[v].push_back({ u, d });
-		g[u].push_back({ v, d });
-	}
-
-	bfs(1);
-	bfs(max_element(begin(vt), end(vt)) - begin(vt));
-	cout << *max_element(begin(vt), end(vt));
+	bt(0);
 }
