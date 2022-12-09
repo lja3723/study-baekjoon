9b0bd645a446a25a0c57fd0c91fa3996edf9f35f
lja3723 <lja3723@gmail.com>
2022-12-09 14:40:57 +0900

    solve 1005, ACM Craft

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,53 +1,46 @@
-﻿//problem No. 2580, 스도쿠
+﻿//problem No. 1005, ACM Craft
 #include <iostream>
 #include <vector>
+#define MAX 1001
 using namespace std;
 
-struct RC { int r, c; };
-int map[9][9], rowCheck[9][10], colCheck[9][10], boxCheck[9][10];
-vector<RC> blank;
+int T, N, K, W, D[MAX], minTime[MAX];
+vector<int> preBuildings[MAX];
 
-int boxIdx(int i, int j) {
-	return 3 * (i / 3) + (j / 3);
+int getMinTime(int V) {
+    if (minTime[V] != -1)
+        return minTime[V];
+
+    if (preBuildings[V].empty())
+        return minTime[V] = D[V];
+
+    int maxTime = -1;
+    for (int u : preBuildings[V])
+        maxTime = max(maxTime, getMinTime(u));
+
+    return minTime[V] = maxTime + D[V];
 }
 
-void checkExist(int i, int j, int n, int flag) {
-	rowCheck[i][n] += flag;
-	colCheck[j][n] += flag;
-	boxCheck[boxIdx(i, j)][n] += flag;
+void solution() {
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) {
+        cin >> D[i];
+        preBuildings[i].clear();
+    }
+    for (int i = 0; i < K; i++) {
+        int X, Y;
+        cin >> X >> Y;
+        preBuildings[Y].push_back(X);
+    }
+    cin >> W;
+
+    fill(minTime, minTime + MAX, -1);
+    cout << getMinTime(W) << "\n";
 }
 
-void bt(int filled) {
-	if (blank.empty()) return;
-	if (filled == blank.size()) {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++)
-				cout << map[i][j] << " ";
-			cout << "\n";
-		}
-		exit(0);
-	}
-
-	RC cur = blank[filled];
-	for (int n = 1; n <= 9; n++) {
-		if (!rowCheck[cur.r][n] && !colCheck[cur.c][n] && !boxCheck[boxIdx(cur.r, cur.c)][n]) {
-			map[cur.r][cur.c] = n;
-			checkExist(cur.r, cur.c, n, 1);
-			bt(filled + 1);
-			map[cur.r][cur.c] = 0;
-			checkExist(cur.r, cur.c, n, -1);
-		}
-	}
-}
-
-int main() {
-	for (int i = 0; i < 9; i++) 
-		for (int j = 0; j < 9; j++) {
-			cin >> map[i][j];
-			checkExist(i, j, map[i][j], 1);
-			if (map[i][j] == 0)
-				blank.push_back({ i, j });
-		}
-
-	bt(0);
+int main()
+{
+    cin.tie(0);
+    cin >> T;
+    while (T--) solution();
 }
