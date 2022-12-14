74d9449ba24a3c38650e90e69f8ebbc5c11e8cda
lja3723 <lja3723@gmail.com>
2022-12-14 13:54:27 +0900

    solve 14503, 로봇 청소기

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,39 +1,47 @@
-﻿//problem No. 1992, 쿼드트리
+﻿//problem No. 14503, 로봇 청소기
 #include <iostream>
 using namespace std;
 
-int N;
-string map[64];
+int N, M;
+int R, C, D; //로봇청소기의 현재 좌표 & 방향
+int map[50][50];
 
-bool isEqual(int bi, int bj, int len) {
-	for (int i = bi; i < bi + len; i++)
-		for (int j = bj; j < bj + len; j++)
-			if (map[i][j] != map[bi][bj])
-				return false;
+//d(X)[i][j]: i방향을 향할때, X쪽의 좌표(r,c) = (j=0, j=1)
+int dLeft[4][2] = { {0, -1}, {-1, 0}, {0, 1}, {1, 0} };
+int dFront[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
 
-	return true;
-}
+int main()
+{
+	cin >> N >> M >> R >> C >> D;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> map[i][j];
 
-string QuadTree(int bi, int bj, int len) {
-	string ret;
-	if (isEqual(bi, bj, len)) {
-		ret = map[bi][bj];
-		return ret;
+	int ans = 0, able = 1;
+	while (1) {
+		if (able) {
+			map[R][C] = -1; //청소함
+			ans++;
+			able = 0;
+		}
+
+		for (int d = 0; !able && d < 4; d++) {
+			if (map[R + dLeft[D][0]][C + dLeft[D][1]] == 0)
+				able = 1;
+
+			if (--D == -1) D = 3; //turn left
+		}
+
+		if (able) {
+			R += dFront[D][0];
+			C += dFront[D][1];
+		}
+		else if (map[R - dFront[D][0]][C - dFront[D][1]] != 1) {
+			R -= dFront[D][0];
+			C -= dFront[D][1];
+		}
+		else break;
 	}
 
-	ret = "(";
-	len /= 2;
-	for (int i = 0; i < 2; i++)
-		for (int j = 0; j < 2; j++)
-			ret += QuadTree(bi + i * len, bj + j * len, len);
-
-	return ret + ")";
-}
-
-int main() {
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> map[i];
-
-	cout << QuadTree(0, 0, N);
+	cout << ans;
 }
