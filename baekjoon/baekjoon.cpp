3d78e3d6669db2571cdca9810d017a2940367e70
lja3723 <lja3723@gmail.com>
2022-12-18 00:04:33 +0900

    solve 2573, 빙산

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,35 +1,86 @@
-﻿//problem No. 2644, 촌수계산
+﻿//problem No. 2573, 빙산
 #include <iostream>
-#include <vector>
+#include <list>
 #include <queue>
 using namespace std;
 
-int n, n1, n2, x, y, m, i, d[101];
-vector<int> g[101];
+struct IJ { int i, j; };
+
+list<IJ> ice;
+int map[300][300], tmp[300][300];
+int di[] = { 0, 0, 1, -1 }, dj[] = { 1, -1, 0, 0 };
+int N, M;
 
 int main() {
-	cin >> n >> n1 >> n2 >> m;
-	while (m--) {
-		cin >> x >> y;
-		g[x].push_back(y);
-		g[y].push_back(x);
-	}
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+			if (map[i][j])
+				ice.push_back({ i, j });
+		}
 
-	fill(d, d + 101, -1);
-	queue<int> q;
-	q.push(n1);
-	d[n1] = 0;
+	for (int year = 0;; year++) {
+		//빙산 수 세기 전 방문배열 초기화
+		fill(&tmp[0][0], &tmp[299][299] + 1, 0);
 
-	while (!q.empty()) {
-		x = q.front(); q.pop();
+		//빙산 수 세기(use bfs)
+		int iceCnt = 0;
+		for (auto& e : ice) {
+			if (!tmp[e.i][e.j]) {
+				//빙산이 분리됐으므로 답 출력
+				if (++iceCnt > 1) {
+					cout << year;
+					exit(0);
+				}
 
-		for (int y : g[x]) {
-			if (d[y] == -1) {
-				d[y] = d[x] + 1;
-				q.push(y);
+				queue<IJ> q;
+				q.push(e);
+				tmp[e.i][e.j] = 1;
+
+				while (!q.empty()) {
+					IJ c = q.front(); q.pop();
+
+					for (int d = 0; d < 4; d++) {
+						int ni = c.i + di[d];
+						int nj = c.j + dj[d];
+						if (!tmp[ni][nj] && map[ni][nj] > 0) {
+							q.push({ ni, nj });
+							tmp[ni][nj] = 1;
+						}
+					}
+				}
 			}
 		}
-	}
 
-	cout << d[n2];
+		//녹는 얼음의 양 계산
+		for (auto& e : ice) {
+			tmp[e.i][e.j] = 0;
+
+			for (int d = 0; d < 4; d++) {
+				int ni = e.i + di[d];
+				int nj = e.j + dj[d];
+				if (map[ni][nj] == 0)
+					tmp[e.i][e.j]++;
+			}
+		}
+
+		//얼음을 녹임
+		auto it = ice.begin();
+		while (it != ice.end()) {
+			int i = it->i, j = it->j;
+			map[i][j] -= tmp[i][j];
+			if (map[i][j] <= 0) {
+				map[i][j] = 0;
+				it = ice.erase(it);
+
+				//끝내 얼음이 다 녹았으므로 0 출력
+				if (ice.empty()) {
+					cout << 0;
+					exit(0);
+				}
+			}
+			else it++;
+		}
+	}
 }
