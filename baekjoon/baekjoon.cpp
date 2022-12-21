0a05bd33e0243b726a8dfbdffd1de66934299cc0
lja3723 <lja3723@gmail.com>
2022-12-22 08:59:23 +0900

    solve 14226, 이모티콘

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,80 +1,57 @@
-﻿//problem No. 4179, 불
+﻿//problem No. 14226, 이모티콘
 #include <iostream>
 #include <queue>
 using namespace std;
+#define MAX 1001
 
-const int FR = 0, JH = 1;
+//t초에 act를 했고, dp개가 출력되며, 클립보드에 cp개 있음
+struct Stat { int dp, cp; };
 
-struct IJ { int i, j; };
-
-int R, C;
-int di[] = { 0, 0, 1, -1 }, dj[] = { 1, -1, 0, 0 };
-int vt[2][1000][1000];
-string map[1000];
-queue<IJ> q[2];
-
-
-bool isOut(int i, int j) {
-	return 0 > i || i >= R || 0 > j || j >= C;
-}
+int S, tDp[MAX];
+bool visit[MAX][MAX];
+queue<Stat> q;
 
 int main() {
-	cin >> R >> C;
-	for (int i = 0; i < R; i++) {
-		cin >> map[i];
-		for (int j = 0; j < C; j++) {
-			if (map[i][j] == 'F') {
-				q[FR].push({i, j});
-				vt[FR][i][j] = 1;
-				map[i][j] = '.';
+	fill(tDp, tDp + MAX, -1);
+
+	cin >> S;
+	q.push({ 1, 0 });
+	visit[1][0] = 1;
+
+	int t = 0;
+	while (tDp[S] == -1 && !q.empty()) {
+		int sz = q.size();
+		while (sz--) {
+			Stat s = q.front(); q.pop();
+
+			if (tDp[s.dp] == -1) {
+				tDp[s.dp] = t;
+				if (s.dp == S)
+					break;
 			}
-			else if (map[i][j] == 'J') {
-				q[JH].push({i, j});
-				vt[JH][i][j] = 1;
-				map[i][j] = '.';
+
+			//1.
+			if (!visit[s.dp][s.dp]) {
+				q.push({ s.dp, s.dp });
+				visit[s.dp][s.dp] = 1;
 			}
-		}
-	}
 
-	while (!q[JH].empty()) {
-		int szFR = q[FR].size(), szJH = q[JH].size();
+			//2.
+			int nxtDp = s.dp + s.cp;
+			if (nxtDp < MAX && !visit[nxtDp][s.cp]) {
+				q.push({ nxtDp, s.cp });
+				visit[nxtDp][s.cp] = 1;
+			}
 
-		while (szJH--) {
-			IJ e = q[JH].front(); q[JH].pop();
-			if (vt[FR][e.i][e.j])
-				continue;
-
-			for (int d = 0; d < 4; d++) {
-				int ni = e.i + di[d];
-				int nj = e.j + dj[d];
-
-				if (isOut(ni, nj)) {
-					cout << vt[JH][e.i][e.j];
-					exit(0);
-				}
-
-				if (map[ni][nj] == '#' || vt[JH][ni][nj] || vt[FR][ni][nj])
-					continue;
-
-				q[JH].push({ ni, nj });
-				vt[JH][ni][nj] = vt[JH][e.i][e.j] + 1;
+			//3.
+			nxtDp = s.dp - 1;
+			if (nxtDp > 0 && !visit[nxtDp][s.cp]) {
+				q.push({ nxtDp, s.cp });
+				visit[nxtDp][s.cp] = 1;
 			}
 		}
-
-		while (szFR--) {
-			IJ e = q[FR].front(); q[FR].pop();
-			for (int d = 0; d < 4; d++) {
-				int ni = e.i + di[d];
-				int nj = e.j + dj[d];
-
-				if (isOut(ni, nj) || map[ni][nj] == '#' || vt[FR][ni][nj])
-					continue;
-
-				q[FR].push({ ni, nj });
-				vt[FR][ni][nj] = 1;
-			}
-		}		
+		t++;
 	}
 
-	cout << "IMPOSSIBLE";
+	cout << tDp[S];
 }
