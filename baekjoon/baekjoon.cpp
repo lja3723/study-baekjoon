9c53f8368f00e756f5049a2a703385ceecfa2923
lja3723 <lja3723@gmail.com>
2022-12-21 18:28:37 +0900

    solve 4179, 불

--- baekjoon/baekjoon.cpp
+++ baekjoon/baekjoon.cpp
@@ -1,37 +1,80 @@
-﻿//problem No. 24508, 나도리팡
+﻿//problem No. 4179, 불
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-int N, K, T, i;
-vector<int> b;
+const int FR = 0, JH = 1;
+
+struct IJ { int i, j; };
+
+int R, C;
+int di[] = { 0, 0, 1, -1 }, dj[] = { 1, -1, 0, 0 };
+int vt[2][1000][1000];
+string map[1000];
+queue<IJ> q[2];
+
+
+bool isOut(int i, int j) {
+	return 0 > i || i >= R || 0 > j || j >= C;
+}
 
 int main() {
-	cin >> N >> K >> T;
-
-	b.resize(N);
-	for (i = 0; i < N; i++)
-		cin >> b[i];
-	sort(begin(b), end(b), greater<int>());
-
-	int f = 0, l = N - 1;
-	while (f < l && 0 <= T) {
-		int need = K - b[f];
-		if (b[l] <= need) {
-			b[f] += b[l];
-			T -= b[l];
-			b[l--] = 0;
-		}
-		else {
-			b[f++] += need;
-			T -= need;
-			b[l] -= need;
+	cin >> R >> C;
+	for (int i = 0; i < R; i++) {
+		cin >> map[i];
+		for (int j = 0; j < C; j++) {
+			if (map[i][j] == 'F') {
+				q[FR].push({i, j});
+				vt[FR][i][j] = 1;
+				map[i][j] = '.';
+			}
+			else if (map[i][j] == 'J') {
+				q[JH].push({i, j});
+				vt[JH][i][j] = 1;
+				map[i][j] = '.';
+			}
 		}
 	}
 
-	if (T >= 0 && (b[f] == K || b[f] == 0))
-		cout << "YES";
-	else
-		cout << "NO";
+	while (!q[JH].empty()) {
+		int szFR = q[FR].size(), szJH = q[JH].size();
+
+		while (szJH--) {
+			IJ e = q[JH].front(); q[JH].pop();
+			if (vt[FR][e.i][e.j])
+				continue;
+
+			for (int d = 0; d < 4; d++) {
+				int ni = e.i + di[d];
+				int nj = e.j + dj[d];
+
+				if (isOut(ni, nj)) {
+					cout << vt[JH][e.i][e.j];
+					exit(0);
+				}
+
+				if (map[ni][nj] == '#' || vt[JH][ni][nj] || vt[FR][ni][nj])
+					continue;
+
+				q[JH].push({ ni, nj });
+				vt[JH][ni][nj] = vt[JH][e.i][e.j] + 1;
+			}
+		}
+
+		while (szFR--) {
+			IJ e = q[FR].front(); q[FR].pop();
+			for (int d = 0; d < 4; d++) {
+				int ni = e.i + di[d];
+				int nj = e.j + dj[d];
+
+				if (isOut(ni, nj) || map[ni][nj] == '#' || vt[FR][ni][nj])
+					continue;
+
+				q[FR].push({ ni, nj });
+				vt[FR][ni][nj] = 1;
+			}
+		}		
+	}
+
+	cout << "IMPOSSIBLE";
 }
